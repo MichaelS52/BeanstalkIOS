1d69e93761b65a346351e2dfb3e4c1f25d729189
bsylva <sylva@comcast.net>
2015-06-05 21:02:03 -0400

    Add a level 1, level 2 and level 3 to the start screen.

--- Beanstalk/GameScene.swift
+++ Beanstalk/GameScene.swift
@@ -11,33 +11,48 @@
 import SpriteKit
 
 
-let StartButtonName = "startButton"
-
 class GameScene: SKScene {
     
-    var startButton: SKNode?
+    var Level1: SKNode?
+    var Level2: SKNode?
+    var Level3: SKNode?
     
     override func didMoveToView(view: SKView) {
         /* Setup your scene here */
         
-        startButton = childNodeWithName(StartButtonName)
-
-        startButton!.position = CGPoint(x:CGRectGetMidX(self.frame), y:CGRectGetMidY(self.frame));
+        Level1 = childNodeWithName("Level1")
+        Level1!.position = CGPoint(x:CGRectGetMidX(self.frame), y:(CGRectGetMidY(self.frame)+100));
+        Level2 = childNodeWithName("Level2")
+        Level2!.position = CGPoint(x:CGRectGetMidX(self.frame), y:CGRectGetMidY(self.frame));
+        Level3 = childNodeWithName("Level3")
+        Level3!.position = CGPoint(x:CGRectGetMidX(self.frame), y:(CGRectGetMidY(self.frame)-100));
     }
-    func resetButtonScale () {
-        startButton?.xScale = 1.0
-        startButton?.yScale = 1.0
+    func unhighlightButtons () {
+        Level1!.xScale = 1.0
+        Level1!.yScale = 1.0
+        Level2!.xScale = 1.0
+        Level2!.yScale = 1.0
+        Level3!.xScale = 1.0
+        Level3!.yScale = 1.0
+    }
+    func highlightButton (b: SKNode) {
+        b.xScale = 1.1
+        b.yScale = 1.1
     }
     
-    func swoleButton(touches: Set<NSObject>) {
-        
-        
+    func checkButtons(touches: Set<NSObject>) {
         for touch in (touches as! Set<UITouch>) {
             let location = touch.locationInNode(self)
-            if (startButton!.containsPoint(location)) {
-                startButton?.xScale = 1.1
-                startButton?.yScale = 1.1
+            if (Level1!.containsPoint(location)) {
+                highlightButton(Level1!)
             }
+            if (Level2!.containsPoint(location)) {
+                highlightButton(Level1!)
+            }
+            if (Level3!.containsPoint(location)) {
+                highlightButton(Level1!)
+            }
+            
         }
         
     }
@@ -45,27 +60,38 @@ class GameScene: SKScene {
     
     override func touchesBegan(touches: Set<NSObject>, withEvent event: UIEvent) {
         /* Called when a touch begins */
-            swoleButton(touches)
+            checkButtons(touches)
     }
 
     
     override func touchesMoved(touches: Set<NSObject>, withEvent event: UIEvent) {
-        resetButtonScale()
-        swoleButton(touches)
+        unhighlightButtons()
+        checkButtons(touches)
     }
     
     override func touchesEnded(touches: Set<NSObject>, withEvent event: UIEvent) {
-        resetButtonScale()
+        unhighlightButtons()
         for touch in (touches as! Set<UITouch>) {
             let location = touch.locationInNode(self)
-            if (startButton!.containsPoint(location)) {
+            if (Level1!.containsPoint(location)) {
+                let scene = MainGame(size: self.scene!.size)
+                scene.scaleMode = SKSceneScaleMode.AspectFill
+                scene.userData = NSMutableDictionary()
+                scene.userData!["level"] = 1
+                self.scene!.view!.presentScene(scene)
+            }
+            if (Level2!.containsPoint(location)) {
                 let scene = MainGame(size: self.scene!.size)
                 scene.scaleMode = SKSceneScaleMode.AspectFill
                 scene.userData = NSMutableDictionary()
                 scene.userData!["level"] = 2
-                
-                
-                
+                self.scene!.view!.presentScene(scene)
+            }
+            if (Level3!.containsPoint(location)) {
+                let scene = MainGame(size: self.scene!.size)
+                scene.scaleMode = SKSceneScaleMode.AspectFill
+                scene.userData = NSMutableDictionary()
+                scene.userData!["level"] = 3
                 self.scene!.view!.presentScene(scene)
             }
         }
